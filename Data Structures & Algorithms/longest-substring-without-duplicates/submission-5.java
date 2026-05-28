e373a545ea002a9760bacee470c13916a87c505a
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2026-05-28 16:19:31 +0200

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> lookup = new HashSet<>();
+        int max = 0;    
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p2 < s.length()) {
+            while (lookup.contains(s.charAt(p2))) {
+                lookup.remove(s.charAt(p1));
+                p1++;
+            }
+            lookup.add(s.charAt(p2));
+            p2++;
+            max = Math.max(max, p2 - p1);
+        }
+        return max;
+    }
+}
