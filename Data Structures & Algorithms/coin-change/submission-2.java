bfe899f1674b1ff43bf44a9ecc8af1cbf7459189
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2026-07-18 21:08:21 +0200

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    int min = -1;
+    int amount;
+    int[] coins;
+
+    public int coinChange(int[] coins, int amount) {
+        this.amount = amount;
+        this.coins = coins;
+
+        rec(0, 0, 0);
+        
+        return min;
+    }
+
+    private void rec(int i, int currAmount, int currCount) {
+        if (i == coins.length) return;
+        if (currAmount == amount) {
+            if (min == -1 || currCount < min) {
+                min = currCount;
+            }
+            return;
+        }
+
+        if (currAmount > amount) return;
+
+        rec(i, currAmount + coins[i], currCount + 1);
+        rec(i + 1, currAmount, currCount);
+    }
+}
