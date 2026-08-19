4d826151cf20bcdd7ec3222feee7acf9291ee962
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2026-08-19 11:34:02 +0200

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,31 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (min.isEmpty() || min.peek() >= val) {
+            min.push(val);
+        }
+    }
+    
+    public void pop() {
+        int pop = stack.pop();
+        if (min.peek() == pop) {
+            min.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
